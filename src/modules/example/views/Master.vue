dcc4482c5c2671bf7af539b25ea9c9ed4eb77e68
JunseungPark <dpfwl8745@gmail.com>
2022-04-13 13:53:31 +0900

    upload template

---
+++ src/modules/example/views/Master.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>This is Exmaple Master Page.</h2>
+        <span>example/views/Master.vue</span>
+    </div>
+</template>
+<script lang="ts" src="./Master.ts"></script>
